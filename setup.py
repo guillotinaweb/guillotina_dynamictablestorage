760f3c481f955ab79beeb73fe356dad466367f02
vangheem <vangheem@gmail.com>
2019-01-11 10:16:01 -0500

    Back to development: 1.0.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except IOError:
 
 setup(
     name='guillotina_dynamictablestorage',
-    version='1.0.2',
+    version='1.0.3.dev0',
     description='Dynamic storages based on tables '
                 'instead of databases',
     long_description=README,
